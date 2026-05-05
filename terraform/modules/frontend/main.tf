53aa1f660cf2409a56691c9afacc5188ead68536
M. Irwan Asikin <225542013+mirwanasikin@users.noreply.github.com>
2026-05-05 10:27:17 +0700

    fix(frontend): fix typo in condition for S3 Frontend

--- terraform/modules/frontend/main.tf
+++ terraform/modules/frontend/main.tf
@@ -31,7 +31,7 @@ resource "aws_s3_bucket_policy" "main" {
       }
       Action   = "s3:GetObject"
       Resource = "${aws_s3_bucket.main.arn}/*"
-      Conditon = {
+      Condition = {
         StringEquals = {
           "AWS:SourceArn" = var.cloudfront_arn
         }
