2aef07e3e9e1789e28f1107797375978475cc246
M. Irwan Asikin <225542013+mirwanasikin@users.noreply.github.com>
2026-05-05 10:28:06 +0700

    feat(compute): add user_data for compute EC2
    features:
    - Install Docker and Docker Compose
    - Add User to Docker Group

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -9,6 +9,20 @@ resource "aws_instance" "app" {
   iam_instance_profile   = var.instance_profile_name
   vpc_security_group_ids = [var.ec2_sg_id]
 
+  user_data = <<-EOF
+    #!/bin/bash
+    # Install Docker
+    dnf install -y docker
+    systemctl enable --now docker
+    usermod -aG docker ec2-user
+
+    # Install Docker Compose plugin
+    mkdir -p /usr/local/lib/docker/cli-plugins
+    curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 \
+      -o /usr/local/lib/docker/cli-plugins/docker-compose
+    chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
+  EOF
+
   tags = {
     Name = "${var.env}-app-${each.key}"
     Env  = var.env
